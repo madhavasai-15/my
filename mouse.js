8b4a99aa2e97e84f75642e6d79d3728a980d7dbe
Madhava Sai <73011030+madhavasai-15@users.noreply.github.com>
2021-09-11 12:29:35 +0530

    Update mouse.js

--- mouse.js
+++ mouse.js
@@ -3,30 +3,24 @@ var mouse = {
     y: 0,
 }
 
-(function() {
-    document.onmousemove = handleMouseMove;
+document.onmousemove = handleMouseMove;
     function handleMouseMove(event) {
         var eventDoc, doc, body;
 
-        event = event || window.event; // IE-ism
+        event = event || window.event; 
+     if (event.pageX == null && event.clientX != null) {
+        eventDoc = (event.target && event.target.ownerDocument) || document;
+        doc = eventDoc.documentElement;
+        body = eventDoc.body;
 
-        // If pageX/Y aren't available and clientX/Y are,
-        // calculate pageX/Y - logic taken from jQuery.
-        // (This is to support old IE)
-        if (event.pageX == null && event.clientX != null) {
-            eventDoc = (event.target && event.target.ownerDocument) || document;
-            doc = eventDoc.documentElement;
-            body = eventDoc.body;
-
-            event.pageX = event.clientX +
-              (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
-              (doc && doc.clientLeft || body && body.clientLeft || 0);
-            event.pageY = event.clientY +
-              (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
-              (doc && doc.clientTop  || body && body.clientTop  || 0 );
-        }
-
-        mouse.x = event.pageX;
-        mouse.y = event.pageY;        
+        event.pageX = event.clientX +
+        (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+        (doc && doc.clientLeft || body && body.clientLeft || 0);
+        event.pageY = event.clientY +
+        (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+        (doc && doc.clientTop  || body && body.clientTop  || 0 );
     }
-})();
+
+    mouse.x = event.pageX;
+    mouse.y = event.pageY;        
+}
